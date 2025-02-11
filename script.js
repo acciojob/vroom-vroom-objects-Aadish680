ee26152b917ebe3a8584044ef1dcf524bfbb6b35
Aadish Shibu <144556449+Aadish680@users.noreply.github.com>
2025-02-11 10:38:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 // Complete the js code
-function Car(make, model) {
-	this.ma = make;
-	this.mo = model;
-}
+// function Car(make, model) {
+// 	this.ma = make;
+// 	this.mo = model;
+// }
 
-function SportsCar(make, model, topSpeed) {
-	Car.call(this , make , model);
-	this.top = topSpeed;
+// function SportsCar(make, model, topSpeed) {
+// 	Car.call(this , make , model);
+// 	this.top = topSpeed;
 	
-}
+// }
 
-Car.prototype.getMakeModel= function(){
-	return `${this.ma} ${this.mo}`;
+// Car.prototype.getMakeModel= function(){
+// 	return `${this.ma} ${this.mo}`;
 	
-}
+// }
 
-SportsCar.prototype.getTopSpeed= funtion(){
-return`${this.top}`;
-}
+// SportsCar.prototype.getTopSpeed= funtion(){
+// return`${this.top}`;
+// }
 
-SportsCar.prototype = Object.create(Car.prototype);
-SportsCar.prototype.constructor = SportsCar;
+// SportsCar.prototype = Object.create(Car.prototype);
+// SportsCar.prototype.constructor = SportsCar;
 
 // class Car{
 // 	constructor(model ,make){
@@ -43,6 +43,38 @@ SportsCar.prototype.constructor = SportsCar;
 // 	}
 	
 // }
+// Car Class
+class Car {
+    constructor(make, model) {
+        this.make = make;
+        this.model = model;
+    }
+
+    getMakeModel() {
+        return `${this.make} ${this.model}`;
+    }
+}
+
+// SportsCar Class (inherits from Car)
+class SportsCar extends Car {
+    constructor(make, model, topSpeed) {
+        super(make, model); // Call parent constructor (Car)
+        this.topSpeed = topSpeed;
+    }
+
+    getTopSpeed() {
+        return `Top Speed: ${this.topSpeed} mph`;
+    }
+}
+
+// Example usage:
+const car1 = new Car("Toyota", "Corolla");
+console.log(car1.getMakeModel()); // Output: Toyota Corolla
+
+const sportsCar1 = new SportsCar("Ferrari", "488 GTB", 211);
+console.log(sportsCar1.getMakeModel()); // Output: Ferrari 488 GTB
+console.log(sportsCar1.getTopSpeed());  // Output: Top Speed: 211 mph
+
 
 
 
